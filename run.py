88ec4290b0faff556a5e0c4185923e8985966d55
andrew2984 <ma.andrew2984@gmail.com>
2026-03-10 10:39:29 +0000

    Updated run.py to fix gunicorn command on Render

--- run.py
+++ run.py
@@ -7,46 +7,37 @@ Created on Thu Jan  8 09:56:32 2026
 
 import os
 import sys
-from flask import Flask, request, jsonify, render_template
+from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
-
 
 from decouple import config
 from config import DevelopmentConfig, DevServerConfig
 from app import create_app
 from seed import seed
 
-
-app = Flask(__name__,
-    static_folder='src/web',
-    static_url_path='')
-
+# Environment and Config setup
 env = config('FLASK_ENV', default='development')
 
 if env == "development":
-    app.config.from_object(DevelopmentConfig)
+    config_class = DevelopmentConfig
 elif env == "dev_server":
-    app.config.from_object(DevServerConfig)
-    
-connection_string = app.config['CONNECTION_STRING']
+    config_class = DevServerConfig
+else:
+    config_class = DevelopmentConfig
 
+# Create app
+app = create_app()
+app.config.from_object(config_class)
+
+# Db setup
+connection_string = app.config['CONNECTION_STRING']
 engine = create_engine(connection_string, echo=True)
 Session = sessionmaker(bind=engine)
+connect_src = app.config.get('CONNECT_SRC', None)
 
-connect_src = app.config['CONNECT_SRC']
 
 if __name__ == "__main__":
-    # Seed database on first run
-    # db_path = "app.db"
-    # if os.getenv("DATABASE_URL", "").startswith("sqlite:///"):
-    #     db_path = os.getenv("DATABASE_URL").replace("sqlite:///", "")
-
-    # if not os.path.exists(db_path):
-    #     seed(os.getenv("DATABASE_URL", "sqlite:///app.db"))
-
-    app = create_app()
     app.run(debug=True)
-
